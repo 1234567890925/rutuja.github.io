c3bdbeaeb779b7eb1a68a4183ff778d4db5df69a
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2024-12-30 06:52:57 -0800

    Update bio.js

--- bio.js
+++ bio.js
@@ -1,11 +1,25 @@
-function Bio(props){
+function Bio(props) {
     console.log(props);
-    <div className="bio">
-        <h1>Hey Everyone!
-            <p>I {props.name}, am an Electrical and Computer Engineering graduate student at the University of Washington, Seattle.</p>
-            <a href="https://www.linkedin.com/in/rutuja-mandapmalvi/" target="_blank">LinkedIn</a>
-        </h1>
-    </div>
-};
+
+    var bioDiv = document.createElement('div');
+    bioDiv.className = 'bio';
+
+    var h1 = document.createElement('h1');
+    h1.textContent = 'Hey Everyone!';
+
+    var p = document.createElement('p');
+    p.textContent = 'I ' + props.name + ', am an Electrical and Computer Engineering graduate student at the University of Washington, Seattle.';
+
+    var a = document.createElement('a');
+    a.href = 'https://www.linkedin.com/in/rutuja-mandapmalvi/';
+    a.target = '_blank';
+    a.textContent = 'LinkedIn';
+
+    h1.appendChild(p);
+    bioDiv.appendChild(h1);
+    bioDiv.appendChild(a);
+
+    return bioDiv;
+}
 
 export default Bio;
