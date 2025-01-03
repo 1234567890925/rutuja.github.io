f6332393bdde26f26b11239447431e702b58299d
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-01-02 22:04:09 -0800

    Update bio.js

--- bio.js
+++ bio.js
@@ -5,6 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
     <link rel="stylesheet" href="styles.css">
+    <style>
+    background{
+    color: blue;
+    }
+    .section {
+        margin-bottom: 10px;
+    }
+     button{
+         background-color: lightblue;
+         color: white;
+         border: darkblue;
+         border-radius: 5px;
+         padding: 10px 20px;
+         font-size: 16px;        
+         cursor: pointer;
+         display: inline-block; 
+         text-align: center;
+         transition: background-color 0.3s ease, transform 0.2s ease;
+    }
+    button:hover {
+        background-color: #FA86C5;
+        transform: scale(1.05);
+}
+</style>
 </head>
 
 <body>
@@ -14,3 +38,4 @@
         </div>
 </body>
 </html>
+<a href="index.html" class="button">Back to Home</a>
