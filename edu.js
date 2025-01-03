33d28e6bd44923c90ff36a52a9d3d1d3074d7ce0
RUTUJA MANDAPMALVI <76784381+1234567890925@users.noreply.github.com>
2025-01-02 22:17:41 -0800

    Update edu.js

--- edu.js
+++ edu.js
@@ -1,11 +1,41 @@
-function Edu(props){
-    <div className="edu">
-        <h1>Education
-            <p1>{props.university}</p1>
-            <p1>{props.graduationby}</p1>
-            <p1>{props.course}</p1>
-        </h1>
-    </div>
-};
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="styles.css">
+    <style>
+    background{
+    color: blue;
+    }
+    .section {
+        margin-bottom: 10px;
+    }
+     button{
+         background-color: lightblue;
+         color: white;
+         border: darkblue;
+         border-radius: 5px;
+         padding: 10px 20px;
+         font-size: 16px;        
+         cursor: pointer;
+         display: inline-block; 
+         text-align: center;
+         transition: background-color 0.3s ease, transform 0.2s ease;
+    }
+    button:hover {
+        background-color: #FA86C5;
+        transform: scale(1.05);
+}
+</style>
+</head>
 
-export default Edu;
+<body>
+        <div class="section" id="bio">
+            <p>Hi! I am an Electrical and Computer Engineering graduate student at the University of Washington, Seattle</p>
+            <p>For more information, please visit: <a href="https://www.linkedin.com/in/rutuja-mandapmalvi/">my LinkedIn</a> profile</p>
+        </div>
+</body>
+</html>
+<a href="index.html" class="button">Back to Home</a>
